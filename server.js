7355392f4eae67bc587cf49a456dbf65581397a0
unknown <admin34@ccslab526u34.ccs.local>
2023-03-27 14:38:54 +0800

    added test get script

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req,res) => {
+    res.send('ilove cock')
+})
+
 app.listen(8000, ()=>{
     console.log('Listening in port 3000');
 })
