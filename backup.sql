6d7683e3efff122100203ec8d7eef473d1b95b5b
TotalElderBerry <lisondrabrian48@gmail.com>
2023-05-01 19:54:17 +0800

    udpated db

--- backup.sql
+++ backup.sql
@@ -1,13 +1,13 @@
--- MariaDB dump 10.19  Distrib 10.4.25-MariaDB, for Win64 (AMD64)
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
 --
 -- Host: localhost    Database: idancedb
 -- ------------------------------------------------------
--- Server version	8.0.32
+-- Server version	8.0.32-0ubuntu0.22.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `attendance`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `attendance` (
   `student_id` int DEFAULT NULL,
   `live_danceclass_id` int DEFAULT NULL,
@@ -48,7 +48,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `dancebooking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `dancebooking` (
   `dance_booking_id` int NOT NULL AUTO_INCREMENT,
   `payment_id` int DEFAULT NULL,
@@ -62,7 +62,7 @@ CREATE TABLE `dancebooking` (
   CONSTRAINT `DanceBooking_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`payment_id`),
   CONSTRAINT `DanceBooking_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
   CONSTRAINT `DanceBooking_ibfk_3` FOREIGN KEY (`dance_class_id`) REFERENCES `danceclass` (`dance_class_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,6 +71,7 @@ CREATE TABLE `dancebooking` (
 
 LOCK TABLES `dancebooking` WRITE;
 /*!40000 ALTER TABLE `dancebooking` DISABLE KEYS */;
+INSERT INTO `dancebooking` VALUES (1,1,29,8,'PENDING'),(2,2,30,8,'PENDING');
 /*!40000 ALTER TABLE `dancebooking` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,7 +81,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `danceclass`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `danceclass` (
   `dance_class_id` int NOT NULL AUTO_INCREMENT,
   `instructor_id` int DEFAULT NULL,
@@ -97,7 +98,7 @@ CREATE TABLE `danceclass` (
   KEY `payment_details_id` (`payment_details_id`),
   CONSTRAINT `DanceClass_ibfk_1` FOREIGN KEY (`instructor_id`) REFERENCES `instructor` (`instructor_id`),
   CONSTRAINT `DanceClass_ibfk_2` FOREIGN KEY (`payment_details_id`) REFERENCES `paymentdetails` (`payment_details_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -106,6 +107,7 @@ CREATE TABLE `danceclass` (
 
 LOCK TABLES `danceclass` WRITE;
 /*!40000 ALTER TABLE `danceclass` DISABLE KEYS */;
+INSERT INTO `danceclass` VALUES (8,54,'test',NULL,'','',200,'',20,1),(9,54,'test2',NULL,'','',100,'',21,1);
 /*!40000 ALTER TABLE `danceclass` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -115,7 +117,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `instructor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `instructor` (
   `instructor_id` int NOT NULL AUTO_INCREMENT,
   `user_id` varchar(50) DEFAULT NULL,
@@ -125,7 +127,7 @@ CREATE TABLE `instructor` (
   PRIMARY KEY (`instructor_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `Instructor_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -134,7 +136,7 @@ CREATE TABLE `instructor` (
 
 LOCK TABLES `instructor` WRITE;
 /*!40000 ALTER TABLE `instructor` DISABLE KEYS */;
-INSERT INTO `instructor` VALUES (23,'z6Z88m3EHZZs3BPAI9L4q98lRWD3',5,'tiktok','hiphip');
+INSERT INTO `instructor` VALUES (54,'GqxBv47LlRYkg6F0cSua5S53h4Z2',0,'test','hiphip');
 /*!40000 ALTER TABLE `instructor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -144,7 +146,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `like`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `like` (
   `like_id` int NOT NULL AUTO_INCREMENT,
   `dance_class_id` int DEFAULT NULL,
@@ -173,7 +175,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `livedanceclass`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `livedanceclass` (
   `live_danceclass_id` int NOT NULL AUTO_INCREMENT,
   `dance_class_id` int DEFAULT NULL,
@@ -183,7 +185,7 @@ CREATE TABLE `livedanceclass` (
   PRIMARY KEY (`live_danceclass_id`),
   KEY `dance_class_id` (`dance_class_id`),
   CONSTRAINT `LiveDanceClass_ibfk_1` FOREIGN KEY (`dance_class_id`) REFERENCES `danceclass` (`dance_class_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -192,6 +194,7 @@ CREATE TABLE `livedanceclass` (
 
 LOCK TABLES `livedanceclass` WRITE;
 /*!40000 ALTER TABLE `livedanceclass` DISABLE KEYS */;
+INSERT INTO `livedanceclass` VALUES (3,8,'2023-05-02','talambn',20),(4,9,'2023-05-02','Cebu',30);
 /*!40000 ALTER TABLE `livedanceclass` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -201,7 +204,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `payment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `payment` (
   `payment_id` int NOT NULL AUTO_INCREMENT,
   `amount` int DEFAULT NULL,
@@ -209,7 +212,7 @@ CREATE TABLE `payment` (
   `sender_name` text,
   `reference_number` text,
   PRIMARY KEY (`payment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -218,6 +221,7 @@ CREATE TABLE `payment` (
 
 LOCK TABLES `payment` WRITE;
 /*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+INSERT INTO `payment` VALUES (1,200,'04/30/2023','student student','4FcYycZ-9UAP-woFC-OgvD-bkgg2CI'),(2,200,'05/01/2023','brian keith lisondra','zZ20dQN-C8pD-7mQu-TIbc-5BfkPUa');
 /*!40000 ALTER TABLE `payment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -227,14 +231,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `paymentdetails`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `paymentdetails` (
   `payment_details_id` int NOT NULL AUTO_INCREMENT,
   `mode_of_payment` text,
   `account_name` text,
   `account_number` text,
   PRIMARY KEY (`payment_details_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -243,6 +247,7 @@ CREATE TABLE `paymentdetails` (
 
 LOCK TABLES `paymentdetails` WRITE;
 /*!40000 ALTER TABLE `paymentdetails` DISABLE KEYS */;
+INSERT INTO `paymentdetails` VALUES (19,'gcash','brian','12121'),(20,'paypal','tesst','ttt'),(21,'paypal','test2','test');
 /*!40000 ALTER TABLE `paymentdetails` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -252,7 +257,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `rating`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rating` (
   `rating_id` int NOT NULL AUTO_INCREMENT,
   `rating` int DEFAULT NULL,
@@ -281,7 +286,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `recordeddanceclass`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recordeddanceclass` (
   `recorded_danceclass_id` int NOT NULL AUTO_INCREMENT,
   `dance_class_id` int DEFAULT NULL,
@@ -307,7 +312,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `student`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `student` (
   `student_id` int NOT NULL AUTO_INCREMENT,
   `user_id` varchar(50) DEFAULT NULL,
@@ -316,7 +321,7 @@ CREATE TABLE `student` (
   PRIMARY KEY (`student_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `Student_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -325,7 +330,7 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (18,'z6Z88m3EHZZs3BPAI9L4q98lRWD3','Intermediate',1);
+INSERT INTO `student` VALUES (28,'GqxBv47LlRYkg6F0cSua5S53h4Z2','Expert',1),(29,'0BKKaCwlecRCW6NSX2UZsx4nMya2','Intermediate',0),(30,'II7rLlAt8ePjwlZzsEOQ1usM7JU2','Expert',0),(31,'DfTmRA5ftbQooYwlwiGXUcrzQlZ2','Expert',0);
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -335,7 +340,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `user_id` varchar(50) NOT NULL,
   `first_name` text NOT NULL,
@@ -355,7 +360,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('z6Z88m3EHZZs3BPAI9L4q98lRWD3','brian','luisodra','C','C','lisondrabrian@gmail.com','2001-06-14 00:00:00.000Z','');
+INSERT INTO `user` VALUES ('0BKKaCwlecRCW6NSX2UZsx4nMya2','student','student','C','C','student@gmail.com','2001-06-14 00:00:00.000Z',''),('DfTmRA5ftbQooYwlwiGXUcrzQlZ2','jack','jack','C','C','jack@gmail.com','2001-06-14 00:00:00.000Z',''),('GqxBv47LlRYkg6F0cSua5S53h4Z2','insttuctor','instructir','C','C','instructor@gmail.com','2001-06-14 00:00:00.000Z',''),('II7rLlAt8ePjwlZzsEOQ1usM7JU2','brian keith','lisondra','C','C','lisondrabrian48@gmail.com','2001-06-14 00:00:00.000Z','');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -368,4 +373,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-13 12:00:52
+-- Dump completed on 2023-05-01 19:53:00
